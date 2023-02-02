7e86bd4fcae2e2ad4ff2132aca32b3ebd71840d8
Vladimir <smelov.vladimir133@gmail.com>
2023-02-02 20:53:49 +0500

    temp

---
+++ packages/client/src/pages/login/Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Form } from 'react-bootstrap'
+import Header from '../../components/Header'
+
+const Login = () => {
+  return (
+    <>
+    <Header/>
+    <Container className="w-100 m-auto">
+      <Form>
+        test
+      </Form>
+    </Container>
+    </>
+  )
+}
+
+export default Login
