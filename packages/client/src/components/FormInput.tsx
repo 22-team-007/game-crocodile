110b5c10806e21c77448fb5f1e11d94ed630184a
Vladimir <smelov.vladimir133@gmail.com>
2023-02-03 00:03:50 +0500

    add pages: - login - registration
    add components:
    - formInput
    
    add validation constants
    add interfaces

---
+++ packages/client/src/components/FormInput.tsx
@@ -0,0 +1,52 @@
+// React
+import { FC } from 'react'
+// React-Hook-Form
+import { UseFormRegisterReturn } from 'react-hook-form';
+// Bootstrap components
+import { Form } from 'react-bootstrap'
+
+interface FormInputProps {
+  label: string;
+  isInvalid: boolean;
+  isPassword?: boolean;
+  placeholder?: string;
+  register: UseFormRegisterReturn;
+  errorMsg?: string;
+}
+
+/**
+ * @example
+ * <FormInput
+ *   label={'Почта'}
+ *   isInvalid={!!errors?.email}
+ *   register={
+ *     register('email', {
+ *       required: 'Обязательное поле.'
+ *     })
+ *   }
+ *   errorMsg={errors?.email?.message}
+ * />
+ */
+const FormInput: FC<FormInputProps> = ({
+  register,
+  errorMsg,
+  isInvalid,
+  placeholder,
+  label,
+  isPassword,
+  }) => {
+  return (
+    <Form.Group>
+      <Form.Label className='mb-0 mx-1'>{label}</Form.Label>
+      <Form.Control
+        {...register}
+        isInvalid={isInvalid}
+        type={isPassword ? 'password' : 'text'}
+        placeholder={placeholder || label}
+      />
+      <Form.Control.Feedback className={'mt-0'} type='invalid'>{errorMsg}</Form.Control.Feedback>
+    </Form.Group>
+  )
+}
+
+export default FormInput
--- packages/client/src/pages/login/Login.tsx
+++ packages/client/src/pages/login/Login.tsx
@@ -1,17 +1,55 @@
-import React from 'react'
-import { Container, Form } from 'react-bootstrap'
-import Header from '../../components/Header'
+// React-Hook-Form
+import { useForm } from 'react-hook-form'
+// interfaces
+import { LoginData } from '../../types/interfaces'
+// components
+import FormInput from '../../components/FormInput'
+// Bootstrap components
+import { Button, Container, Form } from 'react-bootstrap'
+// Utils
+import { validation } from '../../utils/validation'
 
 const Login = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginData>()
+
+  const onSubmitHandler = (data: LoginData) => {
+    console.log(data)
+  }
+
   return (
-    <>
-    <Header/>
-    <Container className="w-100 m-auto">
-      <Form>
-        test
+    <Container className='vh-100 d-flex justify-content-center align-items-center'>
+      <Form style={{width: '300px'}} noValidate onSubmit={handleSubmit(onSubmitHandler)}>
+        <h3 className='text-center'>Вход</h3>
+
+        <FormInput
+          label={'Логин'}
+          isInvalid={!!errors?.login}
+          register={
+            register('login', {
+              required: 'Обязательное поле.',
+              pattern: validation.login.regExp!,
+            })
+          }
+          errorMsg={errors?.login?.message}
+        />
+
+        <FormInput
+          label={'Пароль'}
+          isInvalid={!!errors?.password}
+          register={
+            register('password', {
+              required: 'Обязательное поле.',
+              pattern: validation.password.regExp!,
+            })
+          }
+          errorMsg={errors?.password?.message}
+        />
+
+        <Button className='w-100 mt-3' type='submit'>Войти</Button>
+        <Button className='w-100' size={'sm'} variant='link'>Нет аккаунта?</Button>
       </Form>
     </Container>
-    </>
   )
 }
 
