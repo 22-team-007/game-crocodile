8d70179de98d7da3bfb1390210ed278fb15f76b1
Zinov Nikita <zinov.nikita2010@yandex.ru>
2023-02-09 13:38:11 +0500

    fix spaces

--- packages/client/src/api/leaderbord.ts
+++ packages/client/src/api/leaderbord.ts
@@ -1,10 +1,11 @@
 import ApiBase from './api_base';
+
+const ourGameHash = 'ldtxqftr';
 type LeaderbordAPIType = {
   add: (chatId:number, userId:number, score:number) => Promise<string>,
   all: (cursor?:number, limit?:number) => Promise<LeaderbordType[]>,
   team: (teamName:string, cursor?:number, limit?:number) => Promise<LeaderbordType[]>,
 }
-const ourGameHash = 'ldtxqftr';
 export default class Leaderbord extends ApiBase implements LeaderbordAPIType {
   public async add (chatId:number, userId:number, score:number):Promise<string> {
     const r = await this.POST('/api/v2/leaderboard', { body: JSON.stringify({
@@ -12,13 +13,16 @@ export default class Leaderbord extends ApiBase implements LeaderbordAPIType {
       ratingFieldName: ourGameHash+'-score',
       teamName: `team${chatId}`
     })});
+
     return await r.text();
   }
+
   public async all (cursor = 0, limit = 100):Promise<LeaderbordType[]> {
     const r = await this.POST('/api/v2/leaderboard/all', { body: JSON.stringify({
       ratingFieldName: ourGameHash+'-score', cursor, limit
     })});
     const arr:{ data: Record<string,number>}[] = await r.json();
+
     return arr.map(x=>{
       return {
         userId: x.data[ourGameHash+'-userId'],
@@ -26,11 +30,13 @@ export default class Leaderbord extends ApiBase implements LeaderbordAPIType {
       }
     })
   }
+
   public async team (teamName:string, cursor = 0, limit = 100):Promise<LeaderbordType[]> {
     const r = await this.POST(`/api/v2/leaderboard/${teamName}`, { body: JSON.stringify({
       ratingFieldName: ourGameHash+'-score', cursor, limit
     })});
     const arr:{ data: Record<string,number>}[] = await r.json();
+
     return arr.map(x=>{
       return {
         userId: x.data[ourGameHash+'-userId'],
@@ -38,4 +44,5 @@ export default class Leaderbord extends ApiBase implements LeaderbordAPIType {
       }
     })
   }
+
 }
