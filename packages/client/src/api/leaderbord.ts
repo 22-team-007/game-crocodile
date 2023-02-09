0817fb3a25273342bbaa5080aedfba44e55831e6
Zinov Nikita <zinov.nikita2010@yandex.ru>
2023-02-09 13:22:19 +0500

    fix url

--- packages/client/src/api/leaderbord.ts
+++ packages/client/src/api/leaderbord.ts
@@ -7,7 +7,7 @@ type LeaderbordAPIType = {
 const ourGameHash = 'ldtxqftr';
 export default class Leaderbord extends ApiBase implements LeaderbordAPIType {
   public async add (chatId:number, userId:number, score:number):Promise<string> {
-    const r = await this.POST('/api/v2/resources', { body: JSON.stringify({
+    const r = await this.POST('/api/v2/leaderboard', { body: JSON.stringify({
       data: { [ourGameHash+'-userId']: userId, [ourGameHash+'-score']: score },
       ratingFieldName: ourGameHash+'-score',
       teamName: `team${chatId}`
@@ -15,7 +15,7 @@ export default class Leaderbord extends ApiBase implements LeaderbordAPIType {
     return await r.text();
   }
   public async all (cursor = 0, limit = 100):Promise<LeaderbordType[]> {
-    const r = await this.POST('/api/v2/resources', { body: JSON.stringify({
+    const r = await this.POST('/api/v2/leaderboard/all', { body: JSON.stringify({
       ratingFieldName: ourGameHash+'-score', cursor, limit
     })});
     const arr:{ data: Record<string,number>}[] = await r.json();
@@ -27,7 +27,7 @@ export default class Leaderbord extends ApiBase implements LeaderbordAPIType {
     })
   }
   public async team (teamName:string, cursor = 0, limit = 100):Promise<LeaderbordType[]> {
-    const r = await this.POST(`/api/v2/resources/${teamName}`, { body: JSON.stringify({
+    const r = await this.POST(`/api/v2/leaderboard/${teamName}`, { body: JSON.stringify({
       ratingFieldName: ourGameHash+'-score', cursor, limit
     })});
     const arr:{ data: Record<string,number>}[] = await r.json();
