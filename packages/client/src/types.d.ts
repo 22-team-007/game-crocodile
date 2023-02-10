71ef69528a5cc6b2619a1819be6b1bc5748e518f
Grigoriy Serebrennikov <sgp900@gmail.com>
2023-02-10 15:00:14 +0700

    fix type file error

--- packages/client/src/types.d.ts
+++ packages/client/src/types.d.ts
@@ -2,31 +2,36 @@ declare type Field = {
   label: string
   type: string
   name: string
-  value?: string,
+  value?: string
   message?: string
   errMessage?: string
   regExp?: RegExp
 }
 declare type SignInParams = {
-  [k:string],
-  login: string,
-  password: string,
+  [k: string]
+  login: string
+  password: string
 }
 declare type SignUpParams = SignInParams & {
-  first_name: string,
-  second_name: string,
-  email: string,
-  phone: string,
+  first_name: string
+  second_name: string
+  email: string
+  phone: string
 }
-declare type UserType = Omit<SignUpParams, "password"> & {
-  id: number,
-  display_name?: string,
-  avatar?: string,
+declare type UserType = Omit<SignUpParams, 'password'> & {
+  id: number
+  display_name?: string
+  avatar?: string
 }
-declare type ProfileParams = Omit<UserType, "id">
+declare type ProfileParams = Omit<UserType, 'id'>
 declare type PasswordParams = {
-  [k:string],
-  oldPassword: string,
-  newPassword: string,
-  repeatPassword?: string,
+  [k: string]
+  oldPassword: string
+  newPassword: string
+  repeatPassword?: string
+}
+
+declare type LeaderType = {
+  id: number
+  score: number
 }
