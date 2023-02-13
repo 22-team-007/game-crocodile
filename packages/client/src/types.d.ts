729885f63d0ac0b5637d2836a488d98d02365de0
Zinov Nikita <zinov.nikita2010@yandex.ru>
2023-02-13 15:52:53 +0500

    fix test

--- packages/client/src/types.d.ts
+++ packages/client/src/types.d.ts
@@ -30,3 +30,42 @@ declare type PasswordParams = {
   newPassword: string,
   repeatPassword?: string,
 }
+declare type SignInParams = {
+  login: string,
+  password: string,
+}
+declare type SignUpParams = SignInParams & {
+  first_name: string,
+  second_name: string,
+  email: string,
+  phone: string,
+}
+declare type UserType =  Omit<SignUpParams, "password"> & {
+  id: number,
+  display_name?: string,
+  avatar?: string,
+}
+declare type ResourceType = {
+  id: number,
+  user_id: number,
+  path: string,
+  filename: string,
+  content_type: string,
+  content_size: number,
+  upload_date: string
+}
+declare type GameType = {
+  id: number,
+  title: string,
+  avatar: string,
+  unread_count?: number,
+  last_message?: {
+    user: UserType,
+    time: string,
+    content: string
+  }
+}
+declare type LeaderType = {
+  id: number,
+  score: number
+}
