e77f5d8825a77ef3b2c448c4808e68ed5e628238
Grigoriy Serebrennikov <sgp900@gmail.com>
2023-03-19 17:34:37 +0700

    clean code

--- packages/client/src/components/ThemeSwitcher/themeSwitcher.tsx
+++ packages/client/src/components/ThemeSwitcher/themeSwitcher.tsx
@@ -3,15 +3,15 @@ import Moon from '../moon'
 import MoonFill from '../moon-fill'
 import { useAppDispatch } from '../../hooks/useAppSelector'
 import { UserThemeAction } from '../../store/actions/types'
-import { setTheme, themeTypes } from '../../store/actions/theme'
+import { setTheme } from '../../store/actions/theme'
 import { useAppSelector } from '../../hooks/useAppSelector'
 import { selectTheme } from '../../store/selectors'
 
 const ThemeSwitcher = () => {
   const appTheme = useAppSelector(selectTheme)
 
-  let dispatch: (arg0: UserThemeAction) => any
-  dispatch = useAppDispatch()
+  const dispatch: (arg0: UserThemeAction) => any
+   = useAppDispatch()
 
   const toggleTheme = () => {
 
