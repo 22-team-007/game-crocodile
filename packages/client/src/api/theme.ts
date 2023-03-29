ca67b852ffb83b72d19ea5a014bb7d88da2daca4
Grigoriy Serebrennikov <sgp900@gmail.com>
2023-03-29 12:25:38 +0700

    clean code

--- packages/client/src/api/theme.ts
+++ packages/client/src/api/theme.ts
@@ -1,9 +1,9 @@
 import ApiBase from './api_base'
 
-type GameAPIType = {
-  getThemes: () => Promise<GameType[]>
+type ThemeAPIType = {
+  getThemes: () => Promise<ThemeType[]>
 }
-export default class Themes extends ApiBase implements GameAPIType {
+export default class Themes extends ApiBase implements ThemeAPIType {
   protected host = 'http://localhost:3001'
   public async getThemes(): Promise<any> {
     const r = await this.GET('/api/theme')
