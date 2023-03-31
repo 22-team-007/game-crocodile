f9ef3a91f08afc7ac5a592e459227eee8e234f03
Zinov Nikita <zinov.nikita2010@yandex.ru>
2023-03-31 18:31:26 +0500

    hide root while loading page

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,10 +4,6 @@ import { Provider } from 'react-redux'
 import store, { persistor } from './store/store'
 import { Index } from './router'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-}
-
 // hydrate page, only when persist store get initial data
 persistor.subscribe(() => {
   const { bootstrapped } = persistor.getState()
