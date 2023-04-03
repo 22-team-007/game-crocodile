5ee2aecc9ebb2a16ae9814b9e688b8fbece70bdd
Grigoriy Serebrennikov <sgp900@gmail.com>
2023-04-03 19:22:31 +0700

    enable serviceWorkers

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux'
 import store, { persistor } from './store/store'
 import { Index } from './router'
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/sw.js')
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+}
 
 const root = document.getElementById('root') as HTMLElement
 const hydrate = window.__staticRouterHydrationData
