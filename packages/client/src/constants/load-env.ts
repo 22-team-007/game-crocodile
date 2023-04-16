e341a90dbd8d25305cb06817e2414cdfb82ab9e8
Grigoriy Serebrennikov <sgp900@gmail.com>
2023-04-16 20:06:13 +0700

    add dotenv loading

--- packages/client/src/constants/load-env.ts
+++ packages/client/src/constants/load-env.ts
@@ -1,6 +1,9 @@
-const API_SERVER_PORT = Number(process.env.API_SERVER_PORT) || 3000
-const API_SERVER_HOST = String(process.env.API_SERVER_HOST) || 'localhost'
-const PRAKTIKUM_HOST = String(process.env.PRAKTIKUM_HOST) || 'ya-praktikum.tech'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const API_SERVER_PORT = process.env.API_SERVER_PORT || 3000
+const API_SERVER_HOST = process.env.API_SERVER_HOST || 'localhost'
+const PRAKTIKUM_HOST = process.env.PRAKTIKUM_HOST || 'ya-praktikum.tech'
 
 export const envConstants = {
   API_SERVER_PORT: JSON.stringify(API_SERVER_PORT),
